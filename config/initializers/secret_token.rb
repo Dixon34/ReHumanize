f9d8624a2ba2927b56ffdfbf16b5ac5f30d889c1
Andy Hull <andyhull3@gmail.com>
2014-05-31 14:50:19 -0400

    adding rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReHumanize::Application.config.secret_key_base = 'c4f66ef28d8cad0ac681dd94a845409bbfa93de2e32a986c2091ac943732534327b61d7e17a1c5e344fd97b59108f33cbc4a3d0eb9c9a82d9ff5ab7aec248dc4'
